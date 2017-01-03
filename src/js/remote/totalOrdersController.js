3883ef904bd265aac98fe8bd94f0ff9e6da47edf
sbhujab <Surya.bhujabal@walmart.com>
2017-01-03 16:00:00 +0530

    template cache added to widgets

---
+++ src/js/remote/totalOrdersController.js
@@ -0,0 +1,13 @@
+'use strict';
+
+define(['app'], function (app) {
+
+    //This controller retrieves data from the customersService and associates it with the $scope
+    //The $scope is ultimately bound to the customers view due to convention followed by the routeResolver
+    app.register.controller('CustomersController', ['$scope', 'config', 'dataService',
+        function ($scope, config, dataService) {
+
+            //Controller code goes here
+            $scope.message= 'This message is fetched remotely from controller A';
+        }]);
+});
--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,42 @@
-var app = angular.module("app",[]);
+var app = angular.module("app",["ngRoute"]);
 
-app.controller("widgetCtrl",['$scope','dashboardServices',function($scope,dashboardServices){
+app.config(
+    function( $controllerProvider, $provide, $compileProvider ) {
+        console.log( "Config method executed." );
+        app._controller = app.controller;
+
+        app.controller = function( name, constructor ) {
+            $controllerProvider.register( name, constructor );
+            return( this );
+        };
+    }
+);
+
+app.controller("widgetCtrl",['$scope','dashboardServices','$templateCache',function($scope,dashboardServices,$templateCache){
     $scope.activeWidget='Total Orders';
 
-
-    $scope.widgets=dashboardServices.getWidgets()
-
+    $scope.widgets=dashboardServices.getWidgets();
+    $scope.cntrlName = 'widget controller';
+    $scope.loadTemplate = function(widget) {
+        var container = $('.widget-preview');
+        // var templateText='<div ng-controller="TempCtrl"> <h2>About</h2> <h3>{{total}}</h3> <p>Testing the total</p> <button ng-click="update()">Update</button> </div> <script> console.log("begin"); console.log(app.controller); app.controller("TempCtrl", function ($scope) { $scope.total = 0; console.log("inside"); $scope.update = function () { $scope.total = $scope.total + 1; }; }); console.log("end") <\/script>';
+        container.html($templateCache.get(widget.template));
+        var newScope = angular.element(container).scope();
+        var compile = angular.element(container).injector().get('$compile');
+        compile($(container).contents())(newScope);
+    }
     $scope.showWidget= function(widget){
-        $scope.activeWidget=widget;
-        $scope.widgetDetails=dashboardServices.getWidgetdetails(widget.widgetId);
-    };
+        console.log(widget.widgetName +" "+ widget.template);
 
+    };
 }]);
+app.controller("totalOrdersCtrl",['$scope','dashboardServices',function($scope,dashboardServices){
+    $scope.cntrlName = 'Total Orders controller';
+}]);
+app.controller("exceptionsCtrl",['$scope','dashboardServices',function($scope,dashboardServices){
+    $scope.cntrlName = 'Exceptions controller';
+}]);
+
 
 app.directive("dashboardPreview",function(){
     return {
@@ -38,25 +63,33 @@ app.factory("dashboardServices",function(){
                 "widgetName": "Orders with Exception",
                 "widgetPriority": "2",
                 "widgetIcon": "exception.ico",
-                "widgetValue": "23"
+                "widgetValue": "23",
+                "template":"orders-with-exception.tpl.html",
+                "widgetPath": "/exceptions"
             }, {
                 "widgetId": "12121",
                 "widgetName": "Total Orders",
                 "widgetPriority": "1",
                 "widgetIcon": "orders.ico",
-                "widgetValue": "119"
+                "widgetValue": "119",
+                "template":"total-orders.tpl.html",
+                "widgetPath": "/totalOrders"
             }, {
                 "widgetId": "12123",
-                "widgetName": "Unassigned Orders",
+                "widgetName": "Orders Due",
                 "widgetPriority": "3",
                 "widgetIcon": "orders.ico",
-                "widgetValue": "115"
+                "widgetValue": "115",
+                "template":"orders-due.tpl.html",
+                "widgetPath": "/unassigned"
             }, {
                 "widgetId": "12124",
-                "widgetName": "Take Action",
+                "widgetName": "Pending Items",
                 "widgetPriority": "4",
                 "widgetIcon": "exception.ico",
-                "widgetValue": "23"
+                "widgetValue": "23",
+                "template":"pending-items.tpl.html",
+                "widgetPath": "/takeAction"
             }];
 
             return widgetData;
@@ -88,3 +121,5 @@ app.factory("dashboardServices",function(){
     }
     return services;
 });
+
+
